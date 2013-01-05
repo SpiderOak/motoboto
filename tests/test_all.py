51ca0fa7f3a9e5cf136d61f5c79c368085b32cb1
dougfort <dougfort@spideroak.com>
2013-01-05 10:41:24 -0500

    Exclude test_upload_interval from test_all: it runs too long.

--- tests/test_all.py
+++ tests/test_all.py
@@ -11,6 +11,8 @@ import unittest
 from tests.test_util import initialize_logging
 
 _test_re = re.compile("test_.+?\.py$", re.IGNORECASE)
+_exclude_list = ["test_upload_interval.py", ]
+_not_excluded_file = lambda f: not f in _exclude_list 
 _filename_to_module = lambda f: os.path.splitext(f)[0]
 _load = unittest.defaultTestLoader.loadTestsFromModule  
 
@@ -22,6 +24,7 @@ def _regression_test():
     path = "."
     files = os.listdir(path)                               
     files = filter(_test_re.search, files)                     
+    files = filter(_not_excluded_file, files)                     
     module_names = map(_filename_to_module, files)         
     modules = map(__import__, module_names)                 
     return unittest.TestSuite(map(_load, modules)) 
